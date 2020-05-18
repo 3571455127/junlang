4c0a135c13d1c5aa61bc2653b5ca335ac3c2750a
YuCan21 <13632387731@139.com>
2020-05-18 17:11:44 +0800

    新增社媒微信

--- Cache/Cache/Home/d2a692a1ed3bac784524ff19f482f94d.php
+++ Cache/Cache/Home/d2a692a1ed3bac784524ff19f482f94d.php
@@ -129,18 +129,16 @@
         </nav>
 
     </header>
+<div class="index">
 
+    <!-- banner  -->
+    <div class="banner">
+        <!-- pc -->
+        <div class="pc-banner">
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
 
-    <div class="index">
-
-        <!-- banner  -->
-        <div class="banner">
-            <!-- pc -->
-            <div class="pc-banner">
-                <div class="swiper-container">
-                    <div class="swiper-wrapper">
-
-          <?php  $_result=M("slide_data")->field("*")->where("fid = 1 AND status=1 ")->order("id desc")->limit("5")->select();; if ($_result): $n=0;foreach($_result as $key=>$r):++$n;$mod = ($n % 2 );?><!-- start -->
+                    <?php  $_result=M("slide_data")->field("*")->where("fid = 1 AND status=1 ")->order("id desc")->limit("5")->select();; if ($_result): $n=0;foreach($_result as $key=>$r):++$n;$mod = ($n % 2 );?><!-- start -->
                         <div class="swiper-slide">
                             <a href="#contant">
                                 <img src="<?php echo ($r["pic"]); ?>" alt="<?php echo ($r["title"]); ?>">
@@ -148,21 +146,21 @@
                         </div>
                         <!-- end --><?php endforeach; endif;?>
 
-                    </div>
-                    <div class="swiper-pagination"></div>
-                    <div class="swiper-btn">
-                        <div class="swiper-button-prev swiper-button-white"></div>
-                        <div class="swiper-button-next swiper-button-white"></div>
-                    </div>
-
                 </div>
-            </div>
-            <!-- wap -->
-            <div class="wap-banner">
-                <div class="swiper-container">
-                    <div class="swiper-wrapper">
+                <div class="swiper-pagination"></div>
+                <div class="swiper-btn">
+                    <div class="swiper-button-prev swiper-button-white"></div>
+                    <div class="swiper-button-next swiper-button-white"></div>
+                </div>
 
-          <?php  $_result=M("slide_data")->field("*")->where("fid = 2 AND status=1 ")->order("id desc")->limit("5")->select();; if ($_result): $n=0;foreach($_result as $key=>$r):++$n;$mod = ($n % 2 );?><!-- start -->
+            </div>
+        </div>
+        <!-- wap -->
+        <div class="wap-banner">
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+
+                    <?php  $_result=M("slide_data")->field("*")->where("fid = 2 AND status=1 ")->order("id desc")->limit("5")->select();; if ($_result): $n=0;foreach($_result as $key=>$r):++$n;$mod = ($n % 2 );?><!-- start -->
                         <div class="swiper-slide">
                             <a href="#contant">
                                 <img src="<?php echo ($r["pic"]); ?>" alt="<?php echo ($r["title"]); ?>">
@@ -170,33 +168,33 @@
                         </div>
                         <!-- end --><?php endforeach; endif;?>
 
-                    </div>
-                    <div class="swiper-pagination"></div>
+                </div>
+                <div class="swiper-pagination"></div>
+            </div>
+        </div>
+    </div>
+
+    <!-- about -->
+    <div>
+        <a id="about"></a>
+        <div class="about">
+            <div class="container">
+                <div class="row">
+                    <?php getcatvar('page','id = 74','cont');?>
                 </div>
             </div>
         </div>
+    </div>
 
-        <!-- about -->
-        <div>
-            <a id="about"></a>
-            <div class="about">
-                <div class="container">
-                    <div class="row">
-                       <?php getcatvar('page','id = 74','cont');?>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- product -->
-        <div>
-            <a id="product"></a>
-            <div class="product">
-                <div class="container">
-                    <div class="row">
-                        <h3>Product</h3>
-                        <div class="list clearfix">
-<?php  $_result=M("Product")->field("thumb,title,cont,listorder,id")->where(" 1  AND status=1  AND catid=73")->order("listorder desc")->limit("12")->select();; if ($_result): $i=0;foreach($_result as $key=>$r):++$i;$mod = ($i % 2 );?><!-- start -->
+    <!-- product -->
+    <div>
+        <a id="product"></a>
+        <div class="product">
+            <div class="container">
+                <div class="row">
+                    <h3>Product</h3>
+                    <div class="list clearfix">
+                        <?php  $_result=M("Product")->field("thumb,title,cont,listorder,id")->where(" 1  AND status=1  AND catid=73")->order("listorder desc")->limit("12")->select();; if ($_result): $i=0;foreach($_result as $key=>$r):++$i;$mod = ($i % 2 );?><!-- start -->
                             <div class="col-lg-4 col-md-4 col-sm-6">
                                 <div class="box">
                                     <img src="<?php echo ($r["thumb"]); ?>" alt="<?php echo ($r["title"]); ?>">
@@ -206,108 +204,109 @@
                                 </div>
                             </div>
                             <!-- end --><?php endforeach; endif;?>
-                        </div>
-                        <a class="pro-contact" href="#contact">Contact Us</a>
                     </div>
+                    <a class="pro-contact" href="#contact">Contact Us</a>
                 </div>
             </div>
         </div>
-
-        <!-- Service -->
-        <div>
-            <a id="service"> </a>
-            <div class="service">
-                <div class="container">
-                    <div class="row">
-                       <?php getcatvar('page','id = 108','cont');?>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Partners -->
-        <div>
-            <a id="partner"> </a>
-            <div class="partner">
-                <div class="container">
-                    <div class="row">
-                        <h3>partner</h3>
-                        <!-- pc -->
-                        <div class="pc-partner">
-                       <?php getcatvar('page','id = 77','cont');?>
-                        </div>
-
-                        <!-- wap-partner -->
-                        <div class="wap-partner">
-                       <?php getcatvar('page','id = 77','pcont');?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- contact -->
-        <div>
-            <a id="contact"></a>
-            <div class="contact">
-                <div class="container">
-                    <div class="row">
-                        <div class="clearfix">
-                            <div class="left col-lg-6 col-md-6">
-                                <div class="footer-logo"><img src="__PUBLIC__/www/images/footer-logo.png" alt=""></div>
-                                <div class="ad"><?php echo ($name); ?></div>
-                                <div class="info">
-                                    <ul>
-                                        <li><?php echo ($address); ?></li>
-                                        <li><?php echo ($phonea); ?></li>
-                                        <li><?php echo ($phone); ?></li>
-                                        <li><?php echo ($fox); ?></li>
-                                    </ul>
-                                </div>
-                                <div class="share">
-                                    <ul class="clearfix">
-                                        <li class="linkedin"><a href="<?php echo ($ins); ?>" title=""
-                                                target="_blank" rel="nofollow"><img src="__PUBLIC__/www/images/linkedin.png"
-                                                    alt=""></a>
-                                        </li>
-                                        <li class="twitter"><a href="<?php echo ($twitter); ?>" title=""
-                                                target="_blank" rel="nofollow"><img src="__PUBLIC__/www/images/twitter.png" alt=""></a>
-                                        </li>
-                                        <li class="whatsapp"><a href="<?php echo ($Whatsapp); ?>" title=""
-                                                target="_blank" rel="nofollow"><img src="__PUBLIC__/www/images/whatsapp.png"
-                                                    alt=""></a>
-                                        </li>
-                                        <li class="skype"><a href="<?php echo ($Skype); ?>" title=""
-                                                target="_blank" rel="nofollow"><img src="__PUBLIC__/www/images/skype.png" alt=""></a>
-                                        </li>
-                                        <li class="youtube"><a href="<?php echo ($youtube); ?>" title=""
-                                                target="_blank" rel="nofollow"><img src="__PUBLIC__/www/images/youtube.png" alt=""></a>
-                                        </li>
-                                        <li class="facebook"><a href="<?php echo ($facebook); ?>" title=""
-                                                target="_blank" rel="nofollow"><img src="__PUBLIC__/www/images/facebook.png"
-                                                    alt=""></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="right col-lg-6 col-md-6">
-                                <form class="clearfix" name="form" method="post" onsubmit="return beforeSubmit2(this);" action="index.php?g=Home&a=message">
-                                    <input type="text" placeholder="* Name" name="name">
-                                    <input type="text" placeholder="* Emai" name="email">
-                                    <input type="text" placeholder="Phone Number" name="phone">
-                                    <textarea placeholder="* Message" name="message"></textarea>
-                                    <input type="submit" value="Send" class="submit-btn">
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
     </div>
 
+    <!-- Service -->
+    <div>
+        <a id="service"> </a>
+        <div class="service">
+            <div class="container">
+                <div class="row">
+                    <?php getcatvar('page','id = 108','cont');?>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <!-- Partners -->
+    <div>
+        <a id="partner"> </a>
+        <div class="partner">
+            <div class="container">
+                <div class="row">
+                    <h3>partner</h3>
+                    <!-- pc -->
+                    <div class="pc-partner">
+                        <?php getcatvar('page','id = 77','cont');?>
+                    </div>
+
+                    <!-- wap-partner -->
+                    <div class="wap-partner">
+                        <?php getcatvar('page','id = 77','pcont');?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- contact -->
+    <div>
+        <a id="contact"></a>
+        <div class="contact">
+            <div class="container">
+                <div class="row">
+                    <div class="clearfix">
+                        <div class="left col-lg-6 col-md-6">
+                            <div class="footer-logo"><img src="__PUBLIC__/www/images/footer-logo.png" alt=""></div>
+                            <div class="ad"><?php echo ($name); ?></div>
+                            <div class="info">
+                                <ul>
+                                    <li><?php echo ($address); ?></li>
+                                    <li><?php echo ($phonea); ?></li>
+                                    <li><?php echo ($phone); ?></li>
+                                    <li><?php echo ($fox); ?></li>
+
+                                </ul>
+                            </div>
+                            <div class="share">
+                                <ul class="clearfix">
+                                    <li class="linkedin"><a href="<?php echo ($ins); ?>" title="" target="_blank" rel="nofollow"><img
+                                                src="__PUBLIC__/www/images/linkedin.png" alt=""></a>
+                                    </li>
+                                    <li class="twitter"><a href="<?php echo ($twitter); ?>" title="" target="_blank"
+                                            rel="nofollow"><img src="__PUBLIC__/www/images/twitter.png" alt=""></a>
+                                    </li>
+                                    <li class="whatsapp"><a href="<?php echo ($Whatsapp); ?>" title="" target="_blank"
+                                            rel="nofollow"><img src="__PUBLIC__/www/images/whatsapp.png" alt=""></a>
+                                    </li>
+                                    <li class="skype"><a href="<?php echo ($Skype); ?>" title="" target="_blank" rel="nofollow"><img
+                                                src="__PUBLIC__/www/images/skype.png" alt=""></a>
+                                    </li>
+                                    <li class="youtube"><a href="<?php echo ($youtube); ?>" title="" target="_blank"
+                                            rel="nofollow"><img src="__PUBLIC__/www/images/youtube.png" alt=""></a>
+                                    </li>
+                                    <li class="facebook"><a href="<?php echo ($facebook); ?>" title="" target="_blank"
+                                            rel="nofollow"><img src="__PUBLIC__/www/images/facebook.png" alt=""></a>
+                                    </li>
+                                    <li><a href="javascript:;"><img src="__PUBLIC__/www/images/wechat.png"
+                                                alt="wechat"></a>
+                                        <div class="wechat-box"><img src="__PUBLIC__/www/images/code.png" alt=""></div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="right col-lg-6 col-md-6">
+                            <form class="clearfix" name="form" method="post" onsubmit="return beforeSubmit2(this);"
+                                action="index.php?g=Home&a=message">
+                                <input type="text" placeholder="* Name" name="name">
+                                <input type="text" placeholder="* Emai" name="email">
+                                <input type="text" placeholder="Phone Number" name="phone">
+                                <textarea placeholder="* Message" name="message"></textarea>
+                                <input type="submit" value="Send" class="submit-btn">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
     <!-- footer -->
     <footer>
         <div class="goTop">
